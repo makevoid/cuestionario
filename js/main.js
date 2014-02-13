b569ba56ba22bd142d0680f834902af64adc96de
Francesco Canessa <makevoid@gmail.com>
2014-02-13 06:57:23 +0100

    activities

--- js/main.js
+++ js/main.js
@@ -1,17 +1,56 @@
-project_names = JSON.parse(project_names)
-partners = JSON.parse(partners)
+$(function(){
 
-project_name = document.querySelector("[name=project_name]")
-partners_cont = document.querySelector(".partners")
-project_name.addEventListener("change", function(){
-  partners_sel = document.querySelector("[name=partners]")
-  partners_sel.remove()
-  // append
-  project_name_idx = _(project_names).indexOf(project_name.value)
-  options = ""
-  _(partners[project_name_idx]).each(function(p, idx){
-    options = options+"<option>"+p+"</option>"
+  project_names = JSON.parse(project_names)
+  partners = JSON.parse(partners)
+  activities = JSON.parse(activities)
+
+  project_name = document.querySelector("[name=project_name]")
+  partners_cont = document.querySelector(".partners")
+  project_name.addEventListener("change", function(){
+    sel = document.querySelector("[name=partners]")
+    sel.remove()
+    // append
+    name_idx = _(project_names).indexOf(project_name.value)
+    options = ""
+    _(partners[name_idx]).each(function(p, idx){
+      options = options+"<option>"+p+"</option>"
+    })
+    select = "<select name='partners'>"+options+"</select>"
+    $(partners_cont).append(select)
+
   })
-  select = "<select name='partners'>"+options+"</select>"
-  $(partners_cont).append(select)
+
+  activities_cont = document.querySelector(".activities_cont")
+  project_name.addEventListener("change", function(){
+    sel = document.querySelector("[name=activities]")
+    sel.remove()
+    // append
+    name_idx = _(project_names).indexOf(project_name.value)
+    options = ""
+    _(activities[name_idx]).each(function(p, idx){
+      options = options+"<option>"+p+"</option>"
+    })
+    select = "<select name='activities'>"+options+"</select>"
+    $(activities_cont).append(select)
+    bind_activities()
+
+    // change activity
+    activities_name = document.querySelector("[name=activities]")
+    $(".activities").html(activities_name.value || "...")
+  })
+
+
+  project_name.addEventListener("change", function(){
+    $(".project_name").html(project_name.value)
+  })
+
+  function bind_activities() {
+    activities_name = document.querySelector("[name=activities]")
+    activities_name.addEventListener("change", function(){
+      $(".activities").html(activities_name.value)
+    })
+  }
+
+  bind_activities()
+
 })
--- views/selects.rb
+++ views/selects.rb
@@ -5,7 +5,7 @@ end
 project_names = <<-STR
 Oficina de Coordinación OCO (Coordinada por Diputación de Barcelona)
 Proyecto Fronteras Turísticas (coordinado por Frosinone, Italia)
-Proyecto emiDel (coordinado por L’Hospitalet de Llobregat, España) 
+Proyecto emiDel (coordinado por L’Hospitalet de Llobregat, España)
 Proyecto La basura sirve (coordinado por Arezzo, Italia)
 Proyecto INTEGRATION (coordinado por Stuttgart, Alemania)
 Proyecto MELGODEPRO (coordinado por Alcorcón, España)
@@ -14,12 +14,12 @@ Proyecto Políticas locales de prevención de la violencia (coordinado por Perna
 Proyecto Línea Internacional. Unión de dos pueblos (coordinado por Ponta Porã, Brasil)
 Proyecto COCAP (coordinado por Veneto, Italia)
 Proyecto PACEF (coordinado por Sicilia, Italia)
-Proyecto Innovación Institucional (coordinado por Santa Fe, Argentina) 
+Proyecto Innovación Institucional (coordinado por Santa Fe, Argentina)
 Proyecto Habitar Goes (coordinado por Montevideo, Uruguay)
 Proyecto EU-LA-WIN (coordinado por Emilia-Romagna, Italia)
 Proyecto UNE (coordinado por Irún, España)
 Proyecto Gente diversa, Gente equivalente (coordinado por Santa Tecla, El Salvador)
-Proyecto Gestión Urbana y Territorial Participativa (coordinado por Toscana, Italia) 
+Proyecto Gestión Urbana y Territorial Participativa (coordinado por Toscana, Italia)
 Proyecto RESSOC (coordinado por Área Metropolitana de Barcelona, España)
 Proyecto IDEAL (coordinado por Michoacán, México)
 Proyecto GIT (coordinado por Puerto Cortés, Honduras)
@@ -37,7 +37,7 @@ Asesor/consultor
 STR
 
 partners = [
-%{Diputación de Barcelona (España) 
+%{Diputación de Barcelona (España)
 Fundación Internacional y para Iberoamérica de Administración y Políticas Públicas (FIIAPP, Madrid)
 Región de Toscana (Italia)
 Observatorio Interregional para la Cooperación al Desarrollo (OICS, Roma)
@@ -45,69 +45,69 @@ Municipalidad de San José (Costa Rica)
 Alcaldía Mayor de Bogotá (Colombia)
 Gobierno de la Provincia de Santa Fe (Argentina).},
 
-%{Provincia de Frosinone (Italia) 
-Provincia Autónoma de Bolzano (Italia) 
-Comisión Municipal de Purmamarca (Argentina) 
-Asociación para el Desarrollo Social (ADESO, Argentina) 
-Municipalidad de San Pedro de Quemes y Calacoto (Bolivia) 
-Observatorio Interregional sobre Cooperación al Desarrollo (OICS) 
-Prefectura de Oruro (Bolivia) 
+%{Provincia de Frosinone (Italia)
+Provincia Autónoma de Bolzano (Italia)
+Comisión Municipal de Purmamarca (Argentina)
+Asociación para el Desarrollo Social (ADESO, Argentina)
+Municipalidad de San Pedro de Quemes y Calacoto (Bolivia)
+Observatorio Interregional sobre Cooperación al Desarrollo (OICS)
+Prefectura de Oruro (Bolivia)
 Gobierno de Jujuy (Argentina)},
 
-%{Ayuntamiento de L’Hospitalet (Barcelona, España) 
-Gobierno Autónomo Municipal de La Paz (Bolivia) 
-Alcaldía Municipal de Santa Tecla (El Salvador) 
-Intendencia Municipal de Canelones (Uruguay) 
-Diputación de Barcelona (España) 
+%{Ayuntamiento de L’Hospitalet (Barcelona, España)
+Gobierno Autónomo Municipal de La Paz (Bolivia)
+Alcaldía Municipal de Santa Tecla (El Salvador)
+Intendencia Municipal de Canelones (Uruguay)
+Diputación de Barcelona (España)
 tividades
-Formación de 150 técnicos municipales en desarrollo económico 
+Formación de 150 técnicos municipales en desarrollo económico
 Formación de 150 emprendedores para elaborar sus planes de empresa
-Asistencia técnica a 440 emprendedores mediante atención personalizada. 
-Habilitación de 5 espacios en apoyo a la emprendeduría 
+Asistencia técnica a 440 emprendedores mediante atención personalizada.
+Habilitación de 5 espacios en apoyo a la emprendeduría
 Elaboración de un Documento marco de reflexión sobre emprendedurismo e innovación social
-Desarrollo de una herramienta para la identificación online de buenas prácticas 
+Desarrollo de una herramienta para la identificación online de buenas prácticas
 Implementación de la Estrategia de Mercados Alternativos en La Paz
 Ralización de 8 acciones de mercado social},
 
-%{Municipio de Arezzo (Italia) 
-Municipio de Santiago de Surco (Perú) 
-EMAC – Empresa Pública Municipal de Aseo (Cuenca, Ecuador) 
-Municipalidad de Lago Agrio (Ecuador) 
-Municipalidad General Pico (Argentina) 
-Municipio de Arica (Chile) 
-Gobierno de la Provincia de Buenos Aires (Argentina) 
-Oxfam Italia (Italia) 
-Lille Metropole (Francia) 
-Sociedad Anónima de Economía mixta TRISELEC (Francia) 
-Fundación UOCRA (Argentina) 
-PREMA ECOLOGIA, cooperativa de provisión y exportación para productores de  residuos reciclados limitada (Argentina)},
+%{Municipio de Arezzo (Italia)
+Municipio de Santiago de Surco (Perú)
+EMAC – Empresa Pública Municipal de Aseo (Cuenca, Ecuador)
+Municipalidad de Lago Agrio (Ecuador)
+Municipalidad General Pico (Argentina)
+Municipio de Arica (Chile)
+Gobierno de la Provincia de Buenos Aires (Argentina)
+Oxfam Italia (Italia)
+Lille Metropole (Francia)
+Sociedad Anónima de Economía mixta TRISELEC (Francia)
+Fundación UOCRA (Argentina)
+PREMA ECOLOGIA, cooperativa de provisión y exportación para productores de residuos reciclados limitada (Argentina)},
 
-%{Ciudad de Stuttgart (Alemania) 
-Estado de Chihuahua (México) 
-Ayuntamiento de Guadalajara (México) 
-Alcaldía de Bogotá (Colombia) 
-Municipio del Distrito Metropolitano de Quito (Ecuador) 
-Prefeitura da Cidade de São Paulo (Brasil) 
-Prefeitura da Cidade do Rio de Janeiro (Brasil) 
-KATE – Centro de Desarrollo & Ecología (Alemania) 
-ICLEI – Gobiernos Locales para la Sustentabilidad (Brasil)  Colaboradores 
-Agencia Federal de Medio Ambiente (Alemania) 
-Comisión de Planeación Urbana, Ayuntamiento de Guadalajara (México) 
-Coordinación General de Innovación y Desarrollo del Gobierno de Jalisco (México) 
+%{Ciudad de Stuttgart (Alemania)
+Estado de Chihuahua (México)
+Ayuntamiento de Guadalajara (México)
+Alcaldía de Bogotá (Colombia)
+Municipio del Distrito Metropolitano de Quito (Ecuador)
+Prefeitura da Cidade de São Paulo (Brasil)
+Prefeitura da Cidade do Rio de Janeiro (Brasil)
+KATE – Centro de Desarrollo & Ecología (Alemania)
+ICLEI – Gobiernos Locales para la Sustentabilidad (Brasil) Colaboradores
+Agencia Federal de Medio Ambiente (Alemania)
+Comisión de Planeación Urbana, Ayuntamiento de Guadalajara (México)
+Coordinación General de Innovación y Desarrollo del Gobierno de Jalisco (México)
 Instituto para la Política de Transporte y Desarrollo (México)},
 
-%{Ayuntamiento de Alcorcón (Madrid, España) 
-Municipio de Pimampiro (Ecuador) 
-Municipio de Curahuara de Carangas (Oruro, Bolivia) 
-Municipio de Valparaíso (Zacatecas, México) 
-Cámara Municipal de São João da Madeira (Portugal) 
-AMIBE en Curahuara. Asociación de Emigrantes (Bolivia) 
-Asociación de amas de casa por el desarrollo en Pimampiro. Familiares de  emigrantes (Ecuador) 
-Asociación Hispano Ecuatoriana Rumiñahui (España) 
-Asociación Hispano Boliviana ACOBE (España) 
+%{Ayuntamiento de Alcorcón (Madrid, España)
+Municipio de Pimampiro (Ecuador)
+Municipio de Curahuara de Carangas (Oruro, Bolivia)
+Municipio de Valparaíso (Zacatecas, México)
+Cámara Municipal de São João da Madeira (Portugal)
+AMIBE en Curahuara. Asociación de Emigrantes (Bolivia)
+Asociación de amas de casa por el desarrollo en Pimampiro. Familiares de emigrantes (Ecuador)
+Asociación Hispano Ecuatoriana Rumiñahui (España)
+Asociación Hispano Boliviana ACOBE (España)
 Federación Española de Municipios y Provincias},
 
-%{Câmara Municipal de Borba (Portugal) 
+%{Câmara Municipal de Borba (Portugal)
 Intendencia Departamental de Rivera (Uruguay)
 Municipio de Alegrete (Brasil) 
 Municipio de Quaraí (Brasil
@@ -115,144 +115,147 @@ Municipio de Rosário do Sul (Brasil)
 Municipio de Santana do Livramento (Brasil)
 Fundación Maronna (Brasil)  },
 
-%{Estado de Pernambuco (Brasil) 
-Intendencia de Paysandú (Uruguay) 
-Gobierno de la Región Loreto (Perú) 
-Cesvi fundación (Italia) 
+%{Estado de Pernambuco (Brasil)
+Intendencia de Paysandú (Uruguay)
+Gobierno de la Región Loreto (Perú)
+Cesvi fundación (Italia)
 Comune di Bergamo (Italia)},
 
-%{Prefeitura de Ponta Porã (Brasil) 
-Municipalidad de Pedro Juan Caballero (Paraguay) 
-Diputación de Málaga (España) 
+%{Prefeitura de Ponta Porã (Brasil)
+Municipalidad de Pedro Juan Caballero (Paraguay)
+Diputación de Málaga (España)
 Paz y Desarrollo ONGD (España)},
 
-%{Regione Veneto (Italia) 
-Provincia de San Juan (Argentina) 
-Estado do Rio Grande do Sul (Brasil) 
-Departamento de Misiones (Paraguay) 
+%{Regione Veneto (Italia)
+Provincia de San Juan (Argentina)
+Estado do Rio Grande do Sul (Brasil)
+Departamento de Misiones (Paraguay)
 Fondazione di Venezia (Italia)},
 
-%{Gobierno de la Provincia de Buenos Aires (Argentina) 
-Gobernación Departamento Central (Paraguay) 
-Alcaldía Sacaba (Bolivia) 
-Alcaldía Vinto (Bolivia) 
-Mancomunidad de la Región Andina de Cochabamba (Bolivia) 
-Asociación de Municipios de Cochabamba (Bolivia) 
-Regione Sicilia (Italia) 
+%{Gobierno de la Provincia de Buenos Aires (Argentina)
+Gobernación Departamento Central (Paraguay)
+Alcaldía Sacaba (Bolivia)
+Alcaldía Vinto (Bolivia)
+Mancomunidad de la Región Andina de Cochabamba (Bolivia)
+Asociación de Municipios de Cochabamba (Bolivia)
+Regione Sicilia (Italia)
 ASAEL – Asociación Aragonesa de Entidades Locales (España)},
 
-%{Gobierno de la Provincia de Santa Fe (Argentina) 
-COAMSS – Consejo de Alcaldes del Área Metropolitana de San Salvador (El  Salvador) 
-Gobierno Regional de Arequipa (Perú) 
-Fundación Nacional para el Desarrollo (El Salvador) 
+%{Gobierno de la Provincia de Santa Fe (Argentina)
+COAMSS – Consejo de Alcaldes del Área Metropolitana de San Salvador (El Salvador)
+Gobierno Regional de Arequipa (Perú)
+Fundación Nacional para el Desarrollo (El Salvador)
 Diputación de Barcelona (España)},
 
-%{Intendencia Municipal de Montevideo (Uruguay) 
-Intendencia Municipal de Florida (Uruguay) 
-Municipalidad de Rosario (Argentina) 
-Alcaldía Municipal de San Salvador (El Salvador) 
-Gobierno Municipal de La Paz (Bolivia) 
-Ayuntamiento de Bilbao (España) 
+%{Intendencia Municipal de Montevideo (Uruguay)
+Intendencia Municipal de Florida (Uruguay)
+Municipalidad de Rosario (Argentina)
+Alcaldía Municipal de San Salvador (El Salvador)
+Gobierno Municipal de La Paz (Bolivia)
+Ayuntamiento de Bilbao (España)
 Ayuntamiento de Santa Cruz de Tenerife (España)},
 
-%{Regione Emilia-Romagna – Servizio Politiche Europee e Relazioni Internazionali (Italia) 
-Gobierno de la Provincia de Buenos Aires (Argentina) 
-Secretaria de Estado da Agricultura e do Abastecimento do Paraná (Brasil) 
-Municipio de General Pueyrredón (Argentina) 
-IDEA – Instituto para el Desarrollo de Antioquia (Colombia) 
-Municipio Mar del Plata (Uruguay) 
-Región Marche (Italia) 
-Comune di Roma – Municipio XVII (Italia) 
-CISP – Comitato Internazionale per lo Sviluppo dei Popoli (Italia) 
-CESTAS – Centro di Educazione Sanitaria e tecnologie appropriate sanitarie (Italia) 
+%{Regione Emilia-Romagna – Servizio Politiche Europee e Relazioni Internazionali (Italia)
+Gobierno de la Provincia de Buenos Aires (Argentina)
+Secretaria de Estado da Agricultura e do Abastecimento do Paraná (Brasil)
+Municipio de General Pueyrredón (Argentina)
+IDEA – Instituto para el Desarrollo de Antioquia (Colombia)
+Municipio Mar del Plata (Uruguay)
+Región Marche (Italia)
+Comune di Roma – Municipio XVII (Italia)
+CISP – Comitato Internazionale per lo Sviluppo dei Popoli (Italia)
+CESTAS – Centro di Educazione Sanitaria e tecnologie appropriate sanitarie (Italia)
 Mancomunidad de la Ribera Alta (España)},
 
-%{Ayuntamiento de Irún (País Vasco, España) 
-Gobierno Regional de Arica Parinacota (Chile) 
-Gobierno Regional de Tacna (Perú) 
-Municipalidad de Santa Rosa de Copán (Honduras) 
-Ayuntamiento de Zapotlán el Grande (México) 
+%{Ayuntamiento de Irún (País Vasco, España)
+Gobierno Regional de Arica Parinacota (Chile)
+Gobierno Regional de Tacna (Perú)
+Municipalidad de Santa Rosa de Copán (Honduras)
+Ayuntamiento de Zapotlán el Grande (México)
 Junta de Castilla y León (España)
-AMHON – Asociación de Municipalidades de Honduras (Honduras) 
-IJALTI – Instituto Jalisciense de Tecnologías de la Información (México) 
-FOSIS – Fondo de Solidaridad e Inversión Social (Chile) 
-FONCODES – Fondo de Cooperación para el Desarrollo Social (Perú) 
+AMHON – Asociación de Municipalidades de Honduras (Honduras)
+IJALTI – Instituto Jalisciense de Tecnologías de la Información (México)
+FOSIS – Fondo de Solidaridad e Inversión Social (Chile)
+FONCODES – Fondo de Cooperación para el Desarrollo Social (Perú)
 Consorcio Transfronterizo Bidasoa-Txingudi (España)},
 
-%{Alcaldía de Santa Tecla (El Salvador) 
-Distrito Metropolitano de Quito (Ecuador) 
+%{Alcaldía de Santa Tecla (El Salvador)
+Distrito Metropolitano de Quito (Ecuador)
 Ayuntamiento de Saint Denis (Francia)},
 
-%{Regione Toscana (Italia) 
-Comunidad Regional Punilla (Argentina) 
-Municipalidad de Viña del Mar (Chile) 
-Asamblea Municipal Poder Popular Centro Habana (Cuba) 
-Alcaldía Municipal de León (Nicaragua) 
-Municipalidad de Antigua (Guatemala) 
-Departamento Provence-Alpes-Côte d’Azur (Francia) 
-Associazione Co.opera (Italia)  Colaboradores 
-Circondario Empolese Valdelsa (Italia) 
-Comune di Follonica (Italia) 
-Comune di Livorno (Italia) 
-Comune di Pontedera (Italia) 
-Comune di Roccastrada (Italia) 
-Comune di Siena (Italia) 
-Grupo para el Desarrollo Integral de la Capital (Cuba) 
+%{Regione Toscana (Italia)
+Comunidad Regional Punilla (Argentina)
+Municipalidad de Viña del Mar (Chile)
+Asamblea Municipal Poder Popular Centro Habana (Cuba)
+Alcaldía Municipal de León (Nicaragua)
+Municipalidad de Antigua (Guatemala)
+Departamento Provence-Alpes-Côte d’Azur (Francia)
+Associazione Co.opera (Italia) Colaboradores
+Circondario Empolese Valdelsa (Italia)
+Comune di Follonica (Italia)
+Comune di Livorno (Italia)
+Comune di Pontedera (Italia)
+Comune di Roccastrada (Italia)
+Comune di Siena (Italia)
+Grupo para el Desarrollo Integral de la Capital (Cuba)
 Agencia Córdoba Turismo – Gobierno de la Provincia de Córdoba (Argentina)},
 
-%{Área Metropolitana de Barcelona (España) 
-Alcaldía Municipal de San Salvador y Área Metropolitana de San Salvador (El  Salvador) 
-Municipalidad Provincial del Callao (Perú) 
-Alcaldía de Managua (Nicaragua) 
-Mairie de Toulouse (Francia) 
+%{Área Metropolitana de Barcelona (España)
+Alcaldía Municipal de San Salvador y Área Metropolitana de San Salvador (El Salvador)
+Municipalidad Provincial del Callao (Perú)
+Alcaldía de Managua (Nicaragua)
+Mairie de Toulouse (Francia)
 Intendencia Municipal de Montevideo (Uruguay)},
 
-%{Gobierno del Estado de Michoacán (México) 
-Intermunicipalidad del Gran S. Miguel de Tucumán (Argentina) 
-Mancomunidad de municipios del Norte Paceño Tropical (Bolivia) 
-Intermunicipalidad Huista (Guatemala) 
-Municipalidad Tuma La Dalia, asociando a Intermunicipalidad AMUPEBLAN (Nicaragua) 
-Centro Intermunicipal para el Tratamiento Integral de Residuos Sólidos -CITIRS-  Monarca de Oriente A.C. (Estado de Michoacán, México) 
-Ayuntamiento de San Sebastián de los Reyes (Madrid, España) 
+%{Gobierno del Estado de Michoacán (México)
+Intermunicipalidad del Gran S. Miguel de Tucumán (Argentina)
+Mancomunidad de municipios del Norte Paceño Tropical (Bolivia)
+Intermunicipalidad Huista (Guatemala)
+Municipalidad Tuma La Dalia, asociando a Intermunicipalidad AMUPEBLAN (Nicaragua)
+Centro Intermunicipal para el Tratamiento Integral de Residuos Sólidos -CITIRS- Monarca de Oriente A.C. (Estado de Michoacán, México)
+Ayuntamiento de San Sebastián de los Reyes (Madrid, España)
 Consejo General de los Altos Pirineos (Francia)},
 
-%{Municipalidad de Puerto Cortés (Honduras) 
-Municipalidad de Tela (Honduras) 
-Municipalidad de Omoa (Honduras) 
-Municipalidad de Puerto Barrios (Guatemala) 
+%{Municipalidad de Puerto Cortés (Honduras)
+Municipalidad de Tela (Honduras)
+Municipalidad de Omoa (Honduras)
+Municipalidad de Puerto Barrios (Guatemala)
 Kadaster (Holanda)},
 
-%{Mancomunidad Trinacional Fronteriza Río Lempa (Guatemala) 
-Mancomunidad Copán Chortí (Guatemala) 
-Mancomunidad de Nororiente (Guatemala) 
-Mancomunidad Lago de Guija (Guatemala) 
-AMVAS – Asociación de Municipios del Valle de Sesecapa (Honduras) 
-Asociación de Municipios Trifinio (El Salvador) 
-Asociación de Municipios Cayaguanca (El Salvador) 
-Diputación Provincial de Huelva (España) 
+%{Mancomunidad Trinacional Fronteriza Río Lempa (Guatemala)
+Mancomunidad Copán Chortí (Guatemala)
+Mancomunidad de Nororiente (Guatemala)
+Mancomunidad Lago de Guija (Guatemala)
+AMVAS – Asociación de Municipios del Valle de Sesecapa (Honduras)
+Asociación de Municipios Trifinio (El Salvador)
+Asociación de Municipios Cayaguanca (El Salvador)
+Diputación Provincial de Huelva (España)
 Región de Lombardia (Italia)},
 ]
 
 activities = [
-  
+
 "",
 
-%{Asistencia a 750 emprendedores turísticos para la adquisición de nuevas capacidades de atención al turista 
+%{Asistencia a 750 emprendedores turísticos para la adquisición de nuevas capacidades de atención al turista
 Incorporación de170 prestadores de servicios turísticos no estatales para formar parte de la gestión integrada de la Ruta Camino Andino
 Puesta en valor 142 sitios turísticos
-Creación de 7 rutas turísticas específicas para conformar la  Ruta Camino Andino 
+Creación de 7 rutas turísticas específicas para conformar la  Ruta Camino Andino
 Creación de 8 centros de acceso a nuevas tecnologías},
 
-%{Formación de 150 técnicos municipales en desarrollo económico 
+%{Formación de 150 técnicos municipales en desarrollo económico
 Formación de 150 emprendedores para elaborar sus planes de empresa
-Asistencia técnica a 440 emprendedores mediante atención personalizada. 
-Habilitación de 5 espacios en apoyo a la emprendeduría 
+Asistencia técnica a 440 emprendedores mediante atención personalizada.
+Habilitación de 5 espacios en apoyo a la emprendeduría
 Elaboración de un Documento marco de reflexión sobre emprendedurismo e innovación social,
-Desarrollo de una herramienta para la identificación online de buenas prácticas 
-Implementación de la Estrategia de Mercados Alternativos en La Paz. 
+Desarrollo de una herramienta para la identificación online de buenas prácticas
+Implementación de la Estrategia de Mercados Alternativos en La Paz.
 Ralización de 8 acciones de mercado social.},
 
+"",
+
 ]
+
 objectives = []
 
 partners    = partners.map{   |str| str_format str }
@@ -264,4 +267,5 @@ objectives  = objectives.map{ |str| str_format str }
   functions:      str_format(functions    ),
   partners:       partners,
   objectives:     objectives,
+  activities:     activities,
 }
