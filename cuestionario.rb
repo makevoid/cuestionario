d3618167c6849c7f803e2964908fbafdef8d87c8
Francesco Canessa <makevoid@gmail.com>
2014-03-13 03:00:04 +0100

    second questionnaire seems bug free, needs refactoring but works

--- cuestionario.rb
+++ cuestionario.rb
@@ -48,11 +48,14 @@ class Cuestionario < Sinatra::Base
     end
 
     def select_def(label, choices, sel_choice, options)
-      tag :select, options do
-        choices.each do |choice|
-          selected = true if choice == sel_choice
-          tag :option, selected: selected do
-            write choice
+      tag :label do
+        write label
+        tag :select, options do
+          choices.each do |choice|
+            selected = true if choice == sel_choice
+            tag :option, selected: selected do
+              write choice
+            end
           end
         end
       end
@@ -118,15 +121,12 @@ class Cuestionario < Sinatra::Base
     raise "testing exception"
   end
 
-  def read_results
-    results = File.read "#{@@path}/db/causes.json"
+  def read_results(name)
+    results = File.read "#{@@path}/db/#{name}.json"
     JSON.parse results
   end
 
-  require 'csv'
-  get "/results.csv" do
-    content_type :csv
-    results = read_results
+  def generate_csv(results)
     CSV.generate(col_sep: ",") do |csv|
       csv << results[0].keys
       for result in results
@@ -135,12 +135,30 @@ class Cuestionario < Sinatra::Base
     end
   end
 
+  # deprecate both
+  require 'csv'
+  get "/results.csv" do
+    content_type :csv
+    results = read_results "causes"
+    generate_csv results
+  end
+
+  require 'csv'
+  get "/results/eurosolar.csv" do
+    content_type :csv
+    results = read_results "eurosolar"
+    generate_csv results
+  end
 
 
   FIELDS = [:nombre, :edad, :sexo, :lugar, :municipio, :provincia, :pais, :profesion, :instruccion, :otra_istruccion, :collaborador_beneficiario, :project_name, :partners, "2c", :activities, "3a", :politics, "3c", :pobolacion_beneficiaria, "3e", "3f", :organizacion, "4b", :puestos, "4d", :apreciacion_efectos, "5b", :efectos, "6b", "6c", "6d", :servicios_prestados, :servicios, "8b", :start_time]
 
+  FIELDS_EUROSOLAR = [:pais, :department, :municipality, :nombre_comunidad, :nombre, :cargo, :pertinencia, :pertinencia_otro, :pertinencia_geo, :pertinencia_geo_otro, :pertinencia_operativa, :temas_trasversales, :temas_trasversales_otro, :sostenibilidad, :sostenibilidad_otro, :eficencia1, :eficencia1_otro, :eficencia2, :eficencia2_otro, :eficencia3, :eficencia3_otro, :eficencia4, :eficencia4_otro, :eficencia5, :eficencia5_otro, :eficacia, :eficacia_otro, :efectos1, :efectos1_otro, :efectos2, :efectos2_otro, :efectos3, :efectos3_otro, :efectos4, :efectos4_otro, :efectos5, :efectos5_otro, :efectos6, :efectos6_otro, :efectos7, :efectos7_otro, :efectos_salud1, :efectos_salud1_otro, :efectos_salud2, :efectos_salud2_otro, :efectos_salud3, :efectos_salud3_otro, :desarrollo1, :desarrollo1_otro, :desarrollo2, :desarrollo2_otro, :energeticos1, :energeticos1_otro, :energeticos2, :energeticos2_otro, :energeticos3, :energeticos3_otro, :impacto, :impacto_otro, :sost_inst, :sost_inst_otro, :sost_social1, :sost_social1_otro, :sost_social2, :sost_social3, :sost_social3_otro, :sost_tecn1, :sost_tecn1_otro, :sost_tecn2, :sost_tecn2_otro, :sost_tecn3, :sost_tecn3_otro, :sost_fin1, :sost_fin1_otro, :sost_fin2, :sost_fin2_otro, :sost_fin3, :sost_fin3_otro, :sost_amb1, :sost_amb1_otro, :sost_amb2, :sost_amb2_otro]
+
+
+  # TODO: refactor like csv
   get "/results.xlsx" do
-    results = read_results
+    results = read_results "causes"
 
     Axlsx::Package.new do |p|
       wb = p.workbook
@@ -158,13 +176,34 @@ class Cuestionario < Sinatra::Base
     send_file 'db/results.xlsx'
   end
 
+  get "/results/eurosolar.xlsx" do
+    results = read_results "eurosolar"
+
+    Axlsx::Package.new do |p|
+      wb = p.workbook
+      wb.add_worksheet(:name => "Cuestionario Eurosolar results") do |sheet|
+        sheet.add_row FIELDS_EUROSOLAR
+        for result in results
+          res = FIELDS_EUROSOLAR.map{ |field| result[field.to_s] }
+          sheet.add_row res
+          # sheet.add_row result.values
+        end
+      end
+      p.serialize 'db/eurosolar.xlsx'
+    end
+
+    send_file 'db/eurosolar.xlsx'
+  end
+
   get "/results" do
     send_file "#{@@path}/db/causes.json"
   end
 
   get "/stats" do
     content_type :json
-    results = read_results
-    { count: results.size, last: results.last["start_time"] }.to_json
+    results = read_results "causes"
+    results_euro = read_results "eurosolar"
+    { count: results.size, last: results.last["start_time"], eurosolar: { count: results_euro.size, last: results_euro.last["start_time"] } }.to_json
   end
+
 end
